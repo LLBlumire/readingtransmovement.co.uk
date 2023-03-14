c63ef404fbce3dec85bdbd70ca8f239826efe022
Lucille L. Blumire <lucy@llblumire.co.uk>
2023-03-14 01:25:47 +0000

    Bypass jekyll in gihub pages deploy

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/style.css");
     eleventyConfig.addPassthroughCopy("src/images");
     eleventyConfig.addPassthroughCopy("src/files");
+    eleventyConfig.addPassthroughCopy("src/.nojekyll");
     return {
         dir: {
             input: "src",
