b53087b060a321dd78d3fff4659a6f261b6ce264
Lucille L. Blumire <lucy@llblumire.co.uk>
2023-03-14 01:30:25 +0000

    Remove jekyll bypass

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,6 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/style.css");
     eleventyConfig.addPassthroughCopy("src/images");
     eleventyConfig.addPassthroughCopy("src/files");
-    eleventyConfig.addPassthroughCopy("src/.nojekyll");
     return {
         dir: {
             input: "src",
