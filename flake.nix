67aeb8ec52f966dfca2f95bb05ceb808100eb9cf
Lucille L. Blumire <lucy@llblumire.co.uk>
2023-07-05 19:25:50 +0100

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils/main";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+        devShells."${system}".default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+                yarn
+            ];
+        };
+    };
+}
